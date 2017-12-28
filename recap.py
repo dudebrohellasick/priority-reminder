557b6b30179448aa7ef7f9d0cd437ca947a47cf2
Brandon Ruiz <34893330+dudebrohellasick@users.noreply.github.com>
2017-12-28 06:50:34 -0800

    Add files via upload

---
+++ recap.py
@@ -0,0 +1,12 @@
+print "Let's get some stuff up on the board."
+print "What are your three priorities?"
+priorities = raw_input()
+print "Whats the one thing you will do for the first one?"
+one_thing = raw_input()
+print "What's the one thing you will do for the second one?"
+one_Thing = raw_input()
+print "\nOk so your going to accomplish your first prioritie by %r and your second priorite by %r" % ( one_thing, one_Thing)
+print "\nWait!!!!! we forgot about the third one you dum ass!"
+print "So how will you accomplish your third goal?"
+third = raw_input()
+print"\n ok so now you know the game plan it is to %r then to %r and then you must %r if you want to be the person you see." % (one_thing, one_Thing, third)
